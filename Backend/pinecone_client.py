3f93b74885e621f28bab6a6adbd81ed230608bfa
himanshu <himanshukoul9@gmail.com>
2025-09-26 23:11:13 +0530

    hosted embedding model within pinecone itself to reduce memory

--- Backend/pinecone_client.py
+++ Backend/pinecone_client.py
@@ -1,40 +1,29 @@
 from pinecone_config import dense_index
-from sentence_transformers import SentenceTransformer
-
-dense_model = SentenceTransformer("intfloat/multilingual-e5-large")
 
 NAMESPACE = "real-estate"
 
-def embed_dense(text):
-    return dense_model.encode(text).tolist()
-
 def upsert_listing(doc_id, text):
-    dense_vec = embed_dense(text)
-    metadata = {"chunk_text": text}
-
-    dense_index.upsert(
+    dense_index.upsert_records(
         namespace=NAMESPACE,
-        vectors=[{"id": doc_id, "values": dense_vec, "metadata": metadata}]
+        records=[{"_id": doc_id,"chunk_text":text}]
     )
 
 def delete_listing(doc_id):
     dense_index.delete(ids=[doc_id], namespace=NAMESPACE)
 
 def hybrid_search(query, top_k=10):
-    dense_vec = embed_dense(query)
 
-    results = dense_index.query(
+    results = dense_index.search(
         namespace=NAMESPACE,
-        vector=dense_vec,
-        top_k=top_k,
-        include_metadata=True
+        query={"inputs": {"text": query}, "top_k": top_k},
+        fields=["chunk_text"]
     )
     return [
         {
-            "_id": match["id"],
-            "_score": match["score"],
-            "chunk_text": match["metadata"].get("chunk_text", "")
+            "_id": hit["_id"],
+            "_score": hit["_score"],
+            "chunk_text": hit["fields"].get("chunk_text", "")
         }
-        for match in results["matches"]
+        for hit in results.result.hits
     ]
     
